80cca9e9bd0c26870da8b5821e4018b6ff5d8cf5
Darío Vidal <96150592+DarioRv@users.noreply.github.com>
2023-10-22 19:24:44 -0300

    add: finished home page design

--- src/app/home/pages/home-page/home-page.component.ts
+++ src/app/home/pages/home-page/home-page.component.ts
@@ -13,11 +13,23 @@ export class HomePageComponent {
     { icon: 'check_circle', label: 'Mejora la asistencia' },
     { icon: 'check_circle', label: 'Todo esta organizado' },
     { icon: 'check_circle', label: 'Ahorra tiempo' },
-    { icon: 'check_circle', label: 'Es gratis y no necesitas TC' },
+    { icon: 'check_circle', label: 'Hasta 15 cursos gratis' },
     { icon: 'check_circle', label: 'Sistema intuitivo' },
+    { icon: 'check_circle', label: 'Podés importar tu lista en formate CSV' },
     { icon: 'check_circle', label: 'Ténes informes en formato CSV' },
     { icon: 'check_circle', label: 'Es segura y escalable' },
+    { icon: 'check_circle', label: 'Mayor flexibilidad' },
+    { icon: 'check_circle', label: 'Agilizá tu clase' },
+    { icon: 'check_circle', label: 'Cancelá cuando quieras' },
+    { icon: 'check_circle', label: 'Soporte 24/7' },
   ];
+  featureBenefits = [
+    { icon: 'view_agenda', label: 'Simple' },
+    { icon: 'edit_calendar', label: 'Organizado' },
+    { icon: 'front_hand', label: 'Asistencia ágil' },
+    { icon: 'paid', label: 'Plan gratis' },
+    { icon: 'credit_card_off', label: 'Registro sin TC' },
+  ]
   stepCardItems: Step[] = [
     { id: 1, title: 'Creá tu curso', description: 'Creá tu curso y cargá la lista de estudiantes.' },
     { id: 2, title: 'Cargá la lista de estudiantes', description: 'Podés cargar la lista de estudiantes de manera manual o importarla desde un archivo CSV.' },

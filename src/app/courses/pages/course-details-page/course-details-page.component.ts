46278d5f16fc6334b418181c053b813ce29c1095
Darío Vidal <96150592+DarioRv@users.noreply.github.com>
2023-12-07 01:14:33 -0300

    fix: course details page   Details:     - fix the possibility that course data could be null     - add a snackbar to show the error message if the course data is null

--- src/app/courses/pages/course-details-page/course-details-page.component.ts
+++ src/app/courses/pages/course-details-page/course-details-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Course } from '../../interfaces/course.interface';
 import { CoursesDataService } from '../../services/courses-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-course-details-page',
@@ -11,15 +12,33 @@ import { CoursesDataService } from '../../services/courses-data.service';
 })
 export class CourseDetailsPageComponent implements OnInit {
 
-  public course?: Course;
+  public course!: Course;
   public isLoading: boolean = true;
 
-  constructor(private activedRouter: ActivatedRoute, private coursesDataService: CoursesDataService) {}
+  constructor(private activedRouter: ActivatedRoute, private coursesDataService: CoursesDataService, private router: Router, private snackbar: MatSnackBar) {}
 
   ngOnInit() {
     this.activedRouter.params.subscribe( ({id}) => {
-      this.coursesDataService.getCourses().subscribe((courses) => {this.course = courses.find((course) => course.id == id)});
+      this.coursesDataService.findCourseById(id).subscribe( course => {
+        if (!course) {
+          this.showSnackbar('El curso que desea ver no existe');
+          this.redirectToCourseListPage();
+          return;
+        }
+        this.course = course;
+      });
       this.isLoading = false;
     });
   }
+
+  redirectToCourseListPage() {
+    this.router.navigate(['/dashboard/courses']);
+  }
+
+  showSnackbar(message: string) {
+    this.snackbar.open(message, 'Ok', {
+      duration: 3000
+    });
+  }
+
 }

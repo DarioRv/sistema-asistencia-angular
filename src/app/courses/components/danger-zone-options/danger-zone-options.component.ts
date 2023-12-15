a4b818869a44754bea7fb19cad05daa871fff81c
Darío Vidal <96150592+DarioRv@users.noreply.github.com>
2023-12-15 17:38:44 -0300

    add: danger zone options are now implemented

--- src/app/courses/components/danger-zone-options/danger-zone-options.component.ts
+++ src/app/courses/components/danger-zone-options/danger-zone-options.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CoursesDataService } from '../../services/courses-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { filter, switchMap } from 'rxjs';
+import { ConfirmDialogComponent } from 'src/app/shared/components/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'danger-zone-options',
@@ -7,5 +13,40 @@ import { Component } from '@angular/core';
   ]
 })
 export class DangerZoneOptionsComponent {
+  @Input({required: true}) courseId!: number;
 
+  constructor(private coursesService: CoursesDataService, private snackbar: MatSnackBar, private router: Router, private dialog: MatDialog) { }
+
+  onDeleteCourse() {
+    const dialogRef = this.dialog.open(
+      ConfirmDialogComponent,
+      {
+        data: {
+          title: '¿Esta seguro?',
+          description: 'Se eliminará el curso',
+          confirmButtonText: 'Eliminar',
+          cancelButtonText: 'Cancelar'
+        }
+      });
+    dialogRef.afterClosed()
+    .pipe(
+      filter(result => result),
+      switchMap( () => this.coursesService.deleteCourseById(this.courseId)),
+      filter(wasDeleted => wasDeleted)
+    )
+    .subscribe(() => {
+      this.showSnackbar('Curso eliminado')
+      this.router.navigate(['/dashboard/courses/list']);
+    });
+  }
+
+  onEditCourse() {
+    this.router.navigate(['/dashboard/courses/edit', this.courseId]);
+  }
+
+  showSnackbar(message: string) {
+    this.snackbar.open(message, 'ok', {
+      duration: 3000,
+    });
+  }
 }

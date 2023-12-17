f9ffed866b64d6840e82561236fccbd0173515ce
Darío Vidal <96150592+DarioRv@users.noreply.github.com>
2023-12-16 22:18:25 -0300

    fix: guide display grid

--- src/app/dashboard/pages/home-page/home-page.component.ts
+++ src/app/dashboard/pages/home-page/home-page.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
     `
     .guias {
       display: grid;
-      grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+      grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
       grid-gap: 1rem;
     }
 

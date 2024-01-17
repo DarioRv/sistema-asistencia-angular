70e97ddb93aaf7f42705d98def0fa60759765c09
Darío Rubén Vidal <96150592+DarioRv@users.noreply.github.com>
2024-01-16 23:34:59 -0300

    add(courses): attendance code generator

--- src/app/courses/components/course-assistance-view/course-assistance-view.component.ts
+++ src/app/courses/components/course-assistance-view/course-assistance-view.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CoursesDataService } from '../../services/courses-data.service';
 
 @Component({
   selector: 'course-assistance-view',
@@ -6,6 +7,31 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class CourseAssistanceViewComponent {
+export class CourseAssistanceViewComponent implements OnInit {
+  @Input({ required: true, alias: 'courseData' })
+  courseId!: number;
+  attendanceCode?: string;
 
+  constructor(private courseData: CoursesDataService) { }
+
+  ngOnInit(): void {
+    this.courseData.findCourseById(this.courseId).subscribe( course => {
+
+    });
+  }
+
+  onChangeAttendanceCode(): void {
+    const newCode = this.generateRandomCode();
+    this.attendanceCode = newCode;
+  }
+
+  generateRandomCode(): string {
+    const length = 4;
+    const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result = '';
+    for (let i = length; i > 0; --i) {
+      result += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return result;
+  }
 }

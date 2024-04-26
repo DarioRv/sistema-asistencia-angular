01528e215ede1e175d954131c7e0f56e4a9c8ad0
Darío Rubén Vidal <96150592+DarioRv@users.noreply.github.com>
2024-04-25 22:41:59 -0300

    add(courses): class schedule list component

---
+++ src/app/courses/components/class-schedule-list/class-schedule-list.component.ts
@@ -0,0 +1,51 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ClassScheduleService } from '../../services/class-schedule.service';
+import { ClassScheduleGet } from '../../interfaces/class-schedule-get.interface';
+import { SnackbarService } from 'src/app/shared/services/snackbar.service';
+
+@Component({
+  selector: 'class-schedule-list',
+  templateUrl: './class-schedule-list.component.html',
+  styles: [],
+})
+export class ClassScheduleListComponent implements OnInit {
+  @Input({ required: true })
+  courseId!: string;
+  schedules: ClassScheduleGet[] = [];
+
+  constructor(
+    private scheduleService: ClassScheduleService,
+    private snackbarService: SnackbarService
+  ) {}
+
+  ngOnInit(): void {
+    this.getSchedules();
+  }
+
+  getSchedules(): void {
+    this.scheduleService.getClassSchedulesByCourseId(this.courseId).subscribe({
+      next: (schedules) => {
+        this.schedules = schedules;
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+
+  deleteSchedule(scheduleId: string): void {
+    this.scheduleService.deleteClassSchedule(scheduleId).subscribe({
+      next: (hasDeleted) => {
+        if (hasDeleted) {
+          this.getSchedules();
+          this.snackbarService.showSnackbar('Horario eliminado correctamente');
+        }
+
+        this.snackbarService.showSnackbar('No se pudo eliminar el horario');
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+}

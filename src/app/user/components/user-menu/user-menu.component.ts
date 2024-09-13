e41d59497e12b1b5b1f6bb25477387079f4f2383
Darío Rubén Vidal <96150592+DarioRv@users.noreply.github.com>
2024-09-12 21:05:56 -0300

    add(dashboard): movile design for user menu

--- src/app/user/components/user-menu/user-menu.component.ts
+++ src/app/user/components/user-menu/user-menu.component.ts
@@ -10,7 +10,7 @@ import { SnackbarService } from 'src/app/shared/services/snackbar.service';
   styles: [
     `
       .menu-item {
-        @apply px-2 hover:bg-slate-200 dark:hover:bg-slate-600 py-2 flex items-center gap-2 cursor-pointer;
+        @apply px-2 md:hover:bg-slate-200 md:dark:hover:bg-slate-600 py-2 flex items-center gap-2 cursor-pointer;
       }
     `,
   ],

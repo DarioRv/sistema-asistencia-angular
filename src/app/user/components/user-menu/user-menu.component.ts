7ae11b4a3fde5384168e12bd0e92d0c599ed95c5
Darío Rubén Vidal <96150592+DarioRv@users.noreply.github.com>
2025-05-01 19:42:28 -0300

    chg(dashboard): rebranding diseño

--- src/app/user/components/user-menu/user-menu.component.ts
+++ src/app/user/components/user-menu/user-menu.component.ts
@@ -10,7 +10,11 @@ import { SnackbarService } from 'src/app/shared/services/snackbar.service';
   styles: [
     `
       .menu-item {
-        @apply px-2 md:hover:bg-slate-200 md:dark:hover:bg-slate-600 py-2 flex items-center gap-2 cursor-pointer;
+        @apply px-2 py-2 flex items-center gap-2 mx-1 rounded-md;
+      }
+
+      .menu-item-interactive {
+        @apply cursor-pointer md:hover:bg-core-primary/20 md:hover:text-core-primary md:dark:hover:bg-slate-600 md:dark:hover:text-white transition-colors duration-200 ease-in-out;
       }
     `,
   ],

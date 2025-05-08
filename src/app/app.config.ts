de58cbd3c75501d345c0d790bebac8560adffd8e
Darío Rubén Vidal <96150592+DarioRv@users.noreply.github.com>
2025-05-08 20:22:52 -0300

    fix: update PrimeNG theme configuration for dark mode support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -30,8 +30,8 @@ export const appConfig: ApplicationConfig = {
         preset: Aura,
         options: {
           prefix: 'p',
-          darkModeSelector: 'light',
-          cssLayer: false,
+          darkModeSelector: '.dark',
+          cssLayer: true,
         },
       },
     }),
